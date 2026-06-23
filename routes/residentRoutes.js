838c4a5e64c30d43b81a18af33f3f5a8755c6824
rachell <albarachel326@gmail.com>
2026-06-23 22:24:37 +0800

    added post route

--- routes/residentRoutes.js
+++ routes/residentRoutes.js
@@ -3,5 +3,6 @@ const router                   = express.Router();
 const residentController  = require('../controllers/residentController');
 
 router.get('/', residentController.getResidents);
+router.post('/', residentController.addResident);
 
 module.exports = router;
