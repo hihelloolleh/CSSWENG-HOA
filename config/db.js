1b1351e0572527e2f4640c618f4f51318948b5e0
rachell <albarachel326@gmail.com>
2026-06-23 17:14:14 +0800

    added dateString change to db

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost', 
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || 'hoa_db'
+    database: process.env.DB_NAME || 'hoa_db',
+    dateStrings: true
 }).promise()
 
 
