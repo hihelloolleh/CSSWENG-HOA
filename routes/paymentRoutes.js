a8c42d437c8ac2b2de3da8572f76954ce2d2e77b
caesium-134 <nina_abogadie@dlsu.edu.ph>
2026-07-19 15:30:25 +0800

    updated payment routes
    Added a new route to render payment page based on type.

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const PaymentController = require('../controllers/paymentController');
 
 router.get('/', PaymentController.getPaymentDashboard);
-router.post('/dues', PaymentController.createDue);
-router.post('/dues/:id/update', PaymentController.updateDue);
-router.post('/dues/:id/delete', PaymentController.deleteDue);
-router.post('/payments', PaymentController.createPayment);
-router.post('/payments/:id/update', PaymentController.updatePayment);
-router.post('/payments/:id/delete', PaymentController.deletePayment);
+
+router.get('/:type', (req, res) => {
+    res.render('payments', {
+        title: 'Payments',
+        activePage: 'payments',
+        pageCSS: 'payments.css',
+        paymentType: req.params.type
+    });
+});
 
 module.exports = router;
