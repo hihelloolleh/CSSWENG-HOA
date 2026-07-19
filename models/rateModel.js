3096747f16af72f67361339a54af1ef6c8b83b43
John Christian Llamas <jcllamas1234@gmail.com>
2026-07-20 01:48:23 +0800

    Add files via upload

--- models/rateModel.js
+++ models/rateModel.js
@@ -1,18 +1,29 @@
 const { pool } = require('../config/db');
 
 module.exports = {
-
-    getActiveRates: async() => {
-
-        const[rows] = await pool.execute('SELECT * FROM rates WHERE end_date IS NULL OR end_date > CURDATE() ORDER BY rate_category ASC');
+    getActiveRates: async () => {
+        const [rows] = await pool.execute(`
+            SELECT rate_id, rate_category, amount 
+            FROM Rates 
+            ORDER BY 
+                FIELD(rate_category, 
+                    'Car',
+                    'Car (More than 6 stickers)',
+                    'Motorcycle',
+                    'Commercial',
+                    'House (Monthly Payment)',
+                    'Lot (Monthly Payment)',
+                    'House (Annual Payment)',
+                    'Lot (Annual Payment)'
+                )
+        `);
         return rows;
     },
 
     updateRate: async (rateId, amount, conn = pool) => {
-
-        await conn.execute('UPDATE Rates SET amount = ? WHERE rate_id = ?', [amount, rateId]);
+        await conn.execute(
+            'UPDATE Rates SET amount = ? WHERE rate_id = ?',
+            [amount, rateId]
+        );
     }
 };
-
-
-
